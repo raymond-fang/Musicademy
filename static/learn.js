c3e9c6796ddb9bcf91846446a2ae7e06de44ac22
raymond-fang <55603742+raymond-fang@users.noreply.github.com>
2022-04-25 04:06:27 -0400

    Added genres and learning elements (Raymond), improved UI design and navbar functionality (Mauricio)

--- static/learn.js
+++ static/learn.js
@@ -1,9 +1,35 @@
+var numGenres = 8;
+
 function loadAudio() {
     let audio = $(data.embedAudio);
+    let audio2 = $(data.embedAudio2);
     $("#audio-wrapper").append(audio);
+    $("#audio-wrapper").append(audio2);
 }
 
 
+// load the next question/ prev question  buttons
+function loadButtons(){
+    numNext = parseInt(data.id) + 1;
+    numPrev = parseInt(data.id) - 1;
+
+    prevQuest= $('<a href="/learn/' + numPrev + '" class="btn btn-primary">  < Previous Genre </a>');
+    nextQuest= $('<a href="/learn/' + numNext + '" class="btn btn-primary">  Next Genre > </a>');
+    if (numPrev <= 0){
+        prevQuest.addClass("disabled");
+        prevQuest.prop("disabled",true);
+    }
+    $("#buttons-wrapper").append(prevQuest);
+    if (numNext <= numGenres) {
+        $("#buttons-wrapper").append(nextQuest);
+    }
+        if (numNext > numGenres) {
+        nextQuiz = $('<a href="/quiz/1' + '" class="btn btn-primary">  Take quiz! > </a>');
+        $("#buttons-wrapper").append(nextQuiz);
+    }
+}
+
 $(document).ready(function(){
     loadAudio();
+    loadButtons();
 })
